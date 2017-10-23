bbb398a495736efed06388101a1f6deea0333842
Chen <jiegec@qq.com>
2017-10-23 16:25:16 +0800

    Clean code

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ tree-ar
 This is a version of `tree` that reaches into .zip files.
 
 Allow recursive .zip i.e. .zip in .zip files.
+
+Require CMake, Boost and libzip.
--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <cstdint>
 #include <vector>
-#include <algorithm>
 #include <boost/filesystem.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <zip.h>
 
 using namespace std;
 using namespace boost::filesystem;
+using namespace boost::algorithm;
 
 struct dir_entry {
     path p;
@@ -19,7 +18,7 @@ dir_entry collect_zip(const path &file, zip_source_t *source) {
     result.p = file;
     zip_t *za = nullptr;
     if (source != nullptr) {
-        zip_error_t err;
+        zip_error_t err = {0};
         zip_error_init(&err);
         za = zip_open_from_source(source, ZIP_RDONLY, &err);
         if (za == nullptr) {
@@ -32,19 +31,19 @@ dir_entry collect_zip(const path &file, zip_source_t *source) {
     }
     struct zip_stat sb = {0};
     zip_stat_init(&sb);
-    if(za == nullptr)
+    if (za == nullptr)
         return result;
     auto num = zip_get_num_entries(za, 0);
     vector<path> files;
-    for (auto i=0;i<num;i++) {
-        if(zip_stat_index(za, i, 0, &sb) == 0) {
+    for (zip_uint64_t i = 0; i < num; i++) {
+        if (zip_stat_index(za, i, 0, &sb) == 0) {
             files.emplace_back(path(sb.name));
         }
     }
-    sort(files.begin(),files.end());
+    sort(files.begin(), files.end());
     auto size = files.size();
     dir_entry *current_dir_entry = &result;
-    for (auto i=0;i<size;i++) {
+    for (auto i = 0; i < size; i++) {
         dir_entry temp;
         temp.p = file / files[i];
         path dir = temp.p.parent_path();
@@ -52,17 +51,17 @@ dir_entry collect_zip(const path &file, zip_source_t *source) {
         if (temp.p.filename() == ".")
             continue;
         if (dir == current_dir_entry->p) {
-            if(boost::algorithm::ends_with(temp.p.filename().string(),".zip")) {
+            if (ends_with(temp.p.filename().string(), ".zip")) {
                 current_dir_entry->children.push_back(collect_zip(temp.p, zip_source_zip(za, za, i, 0, 0, -1)));
             } else {
                 current_dir_entry->children.push_back(temp);
             }
-        } else if (boost::algorithm::starts_with(dir,current_dir_entry->p)){
+        } else if (starts_with(dir, current_dir_entry->p)) {
             dir_entry new_dir;
             new_dir.p = dir;
             current_dir_entry->children.push_back(new_dir);
             new_dir.children.push_back(temp);
-            current_dir_entry = &current_dir_entry->children[current_dir_entry->children.size()-1];
+            current_dir_entry = &current_dir_entry->children[current_dir_entry->children.size() - 1];
         } else {
             current_dir_entry = &result;
             result.children.push_back(temp);
@@ -73,16 +72,15 @@ dir_entry collect_zip(const path &file, zip_source_t *source) {
 }
 
 dir_entry collect(const path &root) {
-    auto dir = directory_iterator();
     dir_entry result;
     result.p = root;
-    for (auto x = directory_iterator(root);x != dir;x++) {
-        auto p = x->path();
+    for (auto x : directory_iterator(root)) {
+        auto p = x.path();
 
         if (is_directory(p)) {
             result.children.push_back(collect(p));
         } else if (is_regular_file(p)) {
-            if(boost::algorithm::ends_with(p.filename().string(),".zip")) {
+            if (boost::algorithm::ends_with(p.filename().string(), ".zip")) {
                 result.children.push_back(collect_zip(p, nullptr));
             } else {
                 dir_entry temp;
@@ -98,17 +96,17 @@ void print(const dir_entry &root, uint32_t depth) {
     static bool last[1024] = {false};
     cout << root.p.filename().string() << endl;
     auto size = root.children.size();
-    for (auto i=0;i<size;i++) {
-        last[depth] = i+1 == size;
-        for (auto i=0;i<depth;i++) {
-            cout << (last[i] ? "    " : "|   ");
+    for (auto i = 0; i < size; i++) {
+        last[depth] = i + 1 == size;
+        for (auto j = 0; j < depth; j++) {
+            cout << (last[j] ? "    " : "|   ");
         }
-        cout << ((i+1==size) ? "`" : "|");
+        cout << ((i + 1 == size) ? "`" : "|");
         cout << "-- ";
         if (root.children[i].children.empty()) {
             cout << root.children[i].p.filename().string() << endl;
         } else {
-            print(root.children[i],depth+1);
+            print(root.children[i], depth + 1);
         }
     }
 }
